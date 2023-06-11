66c446685d74082ad077cdff34c42e0518fcaa6c
Syed Raza <syed.raza011@gmail.com>
2023-06-11 12:35:58 -0400

    Initial Commit

---
+++ GridDemos/GridDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridDemosApp.swift
+//  GridDemos
+//
+//  Created by Syed Raza on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GridDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
